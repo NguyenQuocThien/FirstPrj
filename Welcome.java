26e678ded79837fce408e8354a1ea6659c8aa5a9
NguyenQuocThien <23673831.thien@student.iuh.edu.vn>
2024-08-11 14:22:55 +0700

    FirstPrj

---
+++ Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyen Quoc Thien!");
+        System.out.println("Welcome to the DHKTPM19B!");
+    }
+}
